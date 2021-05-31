3c527686a1e17806aea904dd4aad6b4cd1c6121f
Thounaojam-Bafana <76650032+Thounaojam-Bafana@users.noreply.github.com>
2021-05-31 14:50:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,25 +1,33 @@
-function preload(){}
+noseX = 0;
+noseY = 0;
+function preload(){
+    lipstick_filter = loadImage('https://i.postimg.cc/v8CgLbRZ/l1.png')
+}
 function setup(){
     canvas = createCanvas(300, 300);
     canvas.center();
     video = createCapture(VIDEO);
+    video.size(300,300);
     video.hide();
-    poseNet = ml5.poseNet(video, modelLoaded)
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses);
 }
 function draw(){
     image(video, 0,0,300,300);
+    image(lipstick_filter,noseX,noseY,50,20);
 }
 function take_snapshot(){
-    save("Filtered Image");
+    save("Filtered Image.png");
 }
 function modelLoaded(){
     console.log("Posenet is instialize");
-    poseNet.on('pose',gotposes);
+
 }
-function gotposes(results){
+function gotPoses(results)
+{
     if(results.length > 0){
         console.log(results);
-        console.log("NoseX: " + results[0].pose.nose.x);
-        console.log("NoseY: " + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x-25;
+        noseY =  results[0].pose.nose.y+15;
     }
 }
